6c1675f28ecf7f9d668e325729e7aacf5fe8536d
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-06-22 00:13:54 +0300

    L3

--- lesson3/task1/src/Search.jsx
+++ lesson3/task1/src/Search.jsx
@@ -4,9 +4,7 @@ export default function Search(props)  {
 	return (
   <div className='Search'>
     <h1 className='search__title'>
-      Hello,
-      {props.name}
-      . What to search for you
+      {`Hello, ${props.name}. What to search for you`}
     </h1>
     <div className='search__field'>
       <input type='text' className='search__input' />
