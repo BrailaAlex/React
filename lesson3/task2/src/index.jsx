632c17c8ff25cd756b29d1b83f37ada72c08a6f9
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-06-22 00:10:15 +0300

    task2

--- lesson3/task2/src/index.jsx
+++ lesson3/task2/src/index.jsx
@@ -2,7 +2,7 @@ import "./search.scss";
 import "./index.scss";
 import React from "react";
 import ReactDOM from "react-dom";
-import Search from "./Search"
+import Search from "./Search.jsx"
 
 const rootElem = document.querySelector("#root");
 ReactDOM.render(<Search name='Bob' age={17} />, rootElem);
