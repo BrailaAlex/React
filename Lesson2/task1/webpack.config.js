a290876a8139725c5d4392f54608315fa2717a86
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-06-21 11:55:01 +0300

    L2t2

--- Lesson2/task1/webpack.config.js
+++ Lesson2/task1/webpack.config.js
@@ -1,25 +1,27 @@
+//webpack.congig.js
+
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const webpack = require("webpack");
 
 module.exports = {
     entry: "./src/index.js",
     output: {
-        path: __dirname + '/review_build',
+        filename: "bundle.js"
     },
     module: {
-        rules: [{
-                test: /.css$/,
-                use: ["style-loader", "css-loader"]
-            },
+        rules: [
             {
                 test: /.js$/,
                 use: ["babel-loader"]
             }
-        ],
+        ]
     },
     plugins: [
+        new webpack.ProgressPlugin(),
+        new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
             template: "./src/index.html"
         })
     ]
-}
+};
