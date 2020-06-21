ffcae0d25bf776af68c0e7330f36d53b9f32360d
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-06-22 00:06:49 +0300

    L3

--- lesson3/task2/src/Search.jsx
+++ lesson3/task2/src/Search.jsx
@@ -1,21 +1,4 @@
-// const Greeting = (props) => {
-// 	return (
-//   <div className='Search'>
-//     <h1 className='search__title'>
-//       Hello, 
-//       {' '}
-//       {props.name}
-//       . What to search for you
-//     </h1>
-//     <div className='search__field'>
-//       <input type='text' className='search__input' />
-//       <button className='search__button'>Search</button>
-//     </div>
-//   </div>
-// 	);
-// };
-
-// ReactDOM.render(<Greeting name='Bob' age={17} />, rootElem);
+import React from "react";
 
 export default class Search extends Component {
   render() {
