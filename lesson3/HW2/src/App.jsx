4544c5d9aaddce247274e91b3e198a6712fb5ee5
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-06-22 15:59:46 +0300

    L3

---
+++ lesson3/HW2/src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Profile from './Profile';
+
+const userInfo = {
+  firstName: "John",
+  lastName: "Doe",
+  birthDate: '1991-06-22T11:11:11.819Z',
+  birthPlace: 'London'
+};
+
+const App = () => {
+  return (
+    <Profile
+      userData = {userInfo}
+    />
+  )
+}
+
+export default App;
+ 
+
+
