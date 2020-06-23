4c6433cf2ec4515b5b10066219af6fec8f9e2dee
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-06-23 21:02:15 +0300

    L5

--- Lesson5/task2/src/GoodButton.jsx
+++ Lesson5/task2/src/GoodButton.jsx
@@ -3,8 +3,8 @@ import React from "react";
 
 class GoodButton extends React.Component {
 
-  handleClick() {
-    alert('Good job!');
+  handleClick(e) {
+    alert(e.target.textContent);
   }
 
   render() {
