76fd2c070ce942fbe8db8bdf662ad3f78bdb90d9
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-06-24 09:06:32 +0300

    HW1

---
+++ Lesson5/HW1/src/Toggler.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+
+class Toggler extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      toggle: 'Off',
+    }
+  }
+
+  setBtnText = () => {
+    
+    this.setState({
+      toggle: this.state.toggle === "Off"
+        ? "On"
+        : "Off"
+    })
+  }
+    
+    render() {
+      return (
+        <button
+          className="toggler"
+          onClick={() => this.setBtnText()}>
+          {this.state.toggle}
+        </button>
+      )
+  }
+}
+
+export default Toggler;
+
+
