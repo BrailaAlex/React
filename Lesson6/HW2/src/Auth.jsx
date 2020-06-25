1a5638ecf95eecd00de29d4f8d4275198623ef48
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-06-25 15:40:10 +0300

    L6

--- Lesson6/HW2/src/Auth.jsx
+++ Lesson6/HW2/src/Auth.jsx
@@ -30,20 +30,22 @@ class Auth extends React.Component {
 
   render() {
 
-    const loggerContent = this.state.isLoggedIn
-      ? <Logout onlogout={this.handleLogout} />
-      : !this.state.turnSpinner
-        ? <Login onlogin={this.handleLogin} />
-        : this.state.turnSpinner && <Spinner size={40} />
+    // const loggerContent = this.state.isLoggedIn
+    //   ? <Logout onlogout={this.handleLogout} />
+    //   : !this.state.turnSpinner
+    //     ? <Login onlogin={this.handleLogin} />
+    //     : this.state.turnSpinner && <Spinner size={40} />
 
     return (
-        loggerContent
-          /* this.state.isLoggedIn
+      <>
+        {
+          this.state.isLoggedIn
             ? <Logout onlogout={this.handleLogout} />
-            : !this.state.turnSpinner &&
-              <Login onlogin={this.handleLogin} />
+            /* : !this.state.turnSpinner && */
+            : <Login onlogin={this.handleLogin} />
         }
-        {this.state.turnSpinner && <Spinner size={40} />} */
+        {this.state.turnSpinner && <Spinner size={40} />}
+        </>
     );
   }
 }
