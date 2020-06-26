01a4182c6a55fe0a405f15abc5a253b60e25b2f4
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-06-26 22:39:15 +0300

    L7

--- Lesson7/HW2/src/Pagination.jsx
+++ Lesson7/HW2/src/Pagination.jsx
@@ -18,11 +18,11 @@ const Pagination = ({ goPrev, goNext, currentPage, totalitems, itemsPerPage }) =
   <div className="pagination">
       <button className="btn"
         disabled={isPrevPageAvailable}
-      onClick={goPrev}>{`${isPrevPageAvailable}` || '←'}</button>
+      onClick={goPrev}>{isPrevPageAvailable ? '' : '←'}</button>
       <span className="pagination__page">{currentPage + 1}</span>
       <button className="btn"
         disabled={isNextPageAvailable}
-    onClick={goNext}>{`${isPrevPageAvailable}` || '→'}</button>
+    onClick={goNext}>{isNextPageAvailable ? '' : '→'}</button>
   </div>
   )
 }
