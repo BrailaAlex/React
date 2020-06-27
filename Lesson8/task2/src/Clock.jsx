e824e8d0559896ab217753507e38299ee243be73
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-06-27 22:09:52 +0300

    L8

--- Lesson8/task2/src/Clock.jsx
+++ Lesson8/task2/src/Clock.jsx
@@ -1,28 +1,27 @@
 import React, {Component} from "react";
 import moment from 'moment'
 
-const setOffset = (offset) => {
-    return moment()
-      .utcOffset(offset)
-      .format("h:mm:ss a");
-  }
+const formatTime = offset => {
+  return moment()
+    .utcOffset(offset)
+    .format("h:mm:ss A");
+};
 
 class Clock extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      time: setOffset(this.props.offset),
+      time: formatTime(this.props.offset)
     };
-
   }
 
   componentDidMount() {
     this.intervalId = setInterval(() => {
-          this.setState({
-            time: setOffset(this.props.offset)
-            })
-        }, 1000);
+      this.setState({
+        time: formatTime(this.props.offset)
+      });
+    }, 1000);
   }
 
   componentWillUnmount() {
@@ -35,8 +34,10 @@ class Clock extends Component {
         <div className="clock__location">{this.props.location}</div>
         <div className="clock__time">{this.state.time}</div>
       </div>
-    )
+    );
   }
 }
+
+export default Clock;
 
 export default Clock;
