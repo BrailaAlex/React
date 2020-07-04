c59fb6b80b9df3808f5863427a786c5581c64684
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-07-04 13:54:50 +0300

    L11

--- Lesson11/HW1/src/Expand.jsx
+++ Lesson11/HW1/src/Expand.jsx
@@ -5,38 +5,37 @@ import PropTypes from 'prop-types';
 
 const Expand = ({ isOpen, children, title, onToggleContent }) => {
   
-   return (
+   let content;
+  let arrowDirection = "down";
+  if (isOpen) {
+    content = <div className="expand__content">{children}</div>;
+    arrowDirection = "up";
+  }
+
+  return (
     <div className="expand border">
       <div className="expand__header">
         <span className="expand__title">{title}</span>
-          <button
-            className="expand__toggle-btn"
-            onClick={onToggleContent}
-         >
-           {isOpen ? <FontAwesomeIcon icon={faChevronUp} />
+        <button className="expand__toggle-btn" onClick={onToggleContent}>
+          {isOpen ? <FontAwesomeIcon icon={faChevronUp} />
            : <FontAwesomeIcon icon={faChevronDown} />}
         </button>
       </div>
-       {isOpen
-         ? <div className="expand__content">
-            {children}
-          </div>
-          : null
-      }
+      {content}
     </div>
-  )
+  );
 };
 
-Expand.propTypes = {
-  isOpen: PropTypes.bool,
-  children: PropTypes.element.isRequired,
-  title: PropTypes.string,
-  onToggleContent: PropTypes.func.isRequired
-};
+// Expand.propTypes = {
+//   isOpen: PropTypes.bool,
+//   children: PropTypes.element.isRequired,
+//   title: PropTypes.string,
+//   onToggleContent: PropTypes.func.isRequired
+// };
 
-Expand.defaultProps = {
-  title: '',
-  isOpen: false
-}
+// Expand.defaultProps = {
+//   title: '',
+//   isOpen: false
+// }
 
 export default Expand;
