f5dd5f2e34896fe90a04d6b9e1155c3ae8789470
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-07-05 12:28:58 +0300

    L11

--- Lesson11/HW1/src/Expand.jsx
+++ Lesson11/HW1/src/Expand.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faChevronUp, faChevronDown } from '@fortawesome/free-solid-svg-icons'
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { faChevronUp, faChevronDown } from '@fortawesome/free-solid-svg-icons'
 
 const Expand = ({ isOpen, children, title, onClose }) => {
   let content = isOpen
@@ -14,8 +14,9 @@ const Expand = ({ isOpen, children, title, onClose }) => {
       <div className="expand__header">
         <span className="expand__title">{title}</span>
         <button className="expand__toggle-btn" onClick={onClose}>
-          {isOpen ? <FontAwesomeIcon icon={faChevronUp} />
-            : <FontAwesomeIcon icon={faChevronDown} />}
+          {isOpen ? '∧' : '∨'}
+          {/* {isOpen ? <FontAwesomeIcon icon={faChevronUp} />
+            : <FontAwesomeIcon icon={faChevronDown} />} */}
         </button>
       </div>
       {content}
