5116d7c1a1d61e7c8639f42dbe900326dddf21cf
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-07-14 20:56:22 +0300

    L13

--- Lesson13/task1/src/Contacts.jsx
+++ Lesson13/task1/src/Contacts.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 const Contacts = () => {
   return (
     <div className="page__content">
       <h1>Contacts</h1>
       <p>We are here 🗺</p>
-      <Link to="/">Go home</Link>
+      <Link to="/">To Home</Link>
     </div>
   );
 };
--- Lesson13/task1/src/PageNotFound.jsx
+++ Lesson13/task1/src/PageNotFound.jsx
@@ -1,13 +1,13 @@
 import React from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 const PageNotFound = () => {
   return (
     <div className="page__content">
-      <h1>😢</h1>
+      <h1>404 😢</h1>
       <Link to="/">Go home</Link>
     </div>
-  )
+  );
 };
 
 export default PageNotFound;
