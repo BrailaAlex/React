f8c3dc8fcfcefc376fc2f5f3c4ee5d0d8e340972
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-07-14 20:58:30 +0300

    l13

--- Lesson13/task1/src/Products.jsx
+++ Lesson13/task1/src/Products.jsx
@@ -1,12 +1,11 @@
 import React from "react";
-import { Link } from 'react-router-dom';
-
+import { Link } from "react-router-dom";
 const Products = () => {
   return (
     <div className="page__content">
       <h1>Products</h1>
       <p>We will add some products. Later... 😇</p>
-      <Link to="/contacts">Contacts</Link>
+      <Link to="/contacts">To Contacts</Link>
     </div>
   );
 };
